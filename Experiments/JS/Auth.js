1b461a89995cde88599818f2449be54e86ec2db4
Benjamin Gillmore <bgillmore@hammurabis-code.com>
2016-06-01 14:41:42 -0400

    Updated script and html for new process using web app. Added authorize button and test button for getting different job numbers.

--- Experiments/JS/Auth.js
+++ Experiments/JS/Auth.js
@@ -1,11 +1,11 @@
 var AuthScope = {}
-AuthScope.clientId = "294242731306-rrpi0scsiv9bbteru3kkq97vtdo20am7.apps.googleusercontent.com";
-AuthScope.apiKey = "AIzaSyAS1s958FZcVtCZD9uX8DFAR2-GKlNd3Ik";
+AuthScope.clientId = "829710692515-un86n38lssrt21b3qq7tg5mkq2qhq1lf.apps.googleusercontent.com";
+AuthScope.apiKey = "AIzaSyBdJUFTj4tuQysOaiFwlo73PWhK59f8n3A";
 AuthScope.apiScopes = "https://www.googleapis.com/auth/forms";
-AuthScope.callBack = "?callback=postCallBack";
+AuthScope.callBack = "?callback=receive";
 AuthScope.ScriptObjects = [];
-AuthScope.ScriptObjects.scriptId = "MS7nIL3cEDaqmHH0oRiKTTdXTT3ICWQrs";
-AuthScope.ScriptObjects.ScriptUrl = "http://script.google.com//v1/scripts/" + AuthScope.ScriptObjects.scriptId + ":run";
+AuthScope.ScriptObjects.scriptId = "AKfycbxz0vGRirsyWMKfn1N9Wy1zcXQFHCwmz0OxPEVtz64g1860jPM";
+AuthScope.ScriptObjects.ScriptUrl = "https://script.google.com/a/macros/advantedgetooling.com/s/" + AuthScope.ScriptObjects.scriptId + "/exec";
 //$(function () {
 //    gapi.client.setApiKey(apiKey);
 //    window.setTimeout(checkAuth, 1);
@@ -31,107 +31,61 @@ function handleAuthClick() {
 }
 
 function handleAuthResult(authResult) {
-    var authorizeButton = document.getElementById('authorize-button');
+    //var authorizeButton = document.getElementById('authorize-button');
+    //var formButton = document.getElementById('btnSubmit');
     if (authResult && !authResult.error) {
-        authorizeButton.style.visibility = 'hidden';
-        makeApiCall();
+    	$( ".AS_Initial" ).show();
+    	$('#btnSubmit').click(testClick);
+    	//formButton.onClick = testClick; 
     } else {
         if (authResult.error)
-        authorizeButton.style.visibility = '';
-        authorizeButton.onclick = handleAuthClick;
+        $( ".AS_Authorize" ).show();
+    	$('#authorize-button').click(handleAuthClick);
+        //authorizeButton.onclick = handleAuthClick;
     }
 }
 
-function makeApiCall() {
+function testClick(){
+	var email = $('#txtEmail').val();
+	if (email != ''){
+		makeApiCall(email);
+	}
+	else{
+		alert('Must enter an email value.')
+	}
+}
+
+function handleApiSuccess(e){
+	$(".AS_Success").attr("href", e)
+	$( ".AS_Success" ).show();
+}
+
+function receive(e) {
+    console.log(e);
+}
+
+function makeApiCall(email) {
   
     var params = {
-        "parameters": [
-            "test@advantagetooling.com"
-        ]
+        "userEmail" : email
     };
-    var url = AuthScope.ScriptObjects.ScriptUrl + AuthScope.callBack;
-    var request = jQuery.ajax({
-        crossDomain: true,
-        url: url,
-        method: "GET",
-        dataType: "jsonp",
-        data: params
-    });
-    //var xhr = createCORSRequest('POST', AuthScope.ScriptObjects.SciprUrl);
-    //if (!xhr) {
-    //    throw new Error('CORS not supported');
-    //}
-    //var oauthToken = gapi.auth.getToken();
-    //xhr.setRequestHeader('Authorization',
-    //  'Bearer ' + oauthToken.access_token);
-    //xhr.send(params);
-    //xhr.onreadystatechange = function (oEvent) {
-    //    if (xhr.readyState === 4) {
-    //        if (xhr.status === 200) {
-    //            console.log(xhr.responseText)
-    //        } else {
-    //            console.log("Error", xhr.statusText)
-    //        }
-    //    }
-    //}
-
-    //gapi.client.load('plus', 'v1', function () {
-    //    var request = gapi.client.plus.people.get({
-    //        'userId': 'me'
-    //    });
-    //    request.execute(function (resp) {
-    //        var heading = document.createElement('h4');
-    //        var image = document.createElement('img');
-    //        image.src = resp.image.url;
-    //        heading.appendChild(image);
-    //        heading.appendChild(document.createTextNode(resp.displayName));
-    //        document.getElementById('content').appendChild(heading);
-    //    });
-    //});
+    var url = AuthScope.ScriptObjects.ScriptUrl;
+    var request = jQuery.get(
+        url,
+        params,
+        function(e){
+        	handleApiSuccess(e);
+        },
+        'jsonp'
+    )  
+    .done(function(e) {
+    	console.log( "second success" );
+  	})
+  	.fail(function(e) {
+    	alert( "error" );
+  	})
+  	.always(function() {
+    	console.log( "finished" );
+  	});
 }
 
-function postCallBack(e) {
-    consol.log(e);
-}
-
-//function postCallback() {
-//    if (xhr.readyState == 4 && xhr.status == 200) {
-//        progressDone();   //loading is finished
-//        $('#error').hide();
-//        document.getElementById("table").innerHTML = xmlhttp.responseText;
-
-//        // continue to process post callback.
-//        resetFilters();
-//    }
-//    else {
-//        // report error with fetch
-
-//        /*if(xmlhttp.status==404 || xmlhttp.responseText == "")
-//            $('#error').show();*/
-//        //$('#error').show();
-//    }
-//}
-
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, true);
-
-    } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-
-    } else {
-
-        // Otherwise, CORS is not supported by the browser.
-        xhr = null;
-
-    }
-    return xhr;
-}
